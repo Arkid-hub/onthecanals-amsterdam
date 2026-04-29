8a6f70d75e603c86eb76ae91ad7309453ba6ee2a
Arkid-hub <yurilinthout@gmail.com>
2026-04-29 14:21:59 +0200

    Fix debug import

--- app/api/debug/route.ts
+++ app/api/debug/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { getFeaturedActivities, getAllActivities } from '@/lib/data'
+import { getFeaturedActivities, getAllActivitiesData as getAllActivities } from '@/lib/data'
 
 export const dynamic = 'force-dynamic'
 
